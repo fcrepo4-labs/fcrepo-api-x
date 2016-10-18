bec0198b020359a8c1b745e8fe6ba1414f65704e
Elliot Metsger <emetsger@jhu.edu>
2016-10-18 13:21:43 -0400

    Attempt to put fcrepo-api-x-integration/target into s3 after build failure.

---
+++ s3.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+function putS3
+{
+    objectName=$1
+    file=$objectName
+    bucket=apixtravis
+    resource="/${bucket}/${objectName}"
+    contentType="application/octet-stream"
+    dateValue=$(date +"%a, %d %b %Y %T %z")
+    stringToSign="PUT\n\n${contentType}\n${dateValue}\n${resource}"
+    s3Key=AKIAI2HKU45U4Q6M3YDA
+    s3Secret=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
+    signature=`echo -en ${stringToSign} | openssl sha1 -hmac ${s3Secret} -binary | base64`
+    curl -v -i -X PUT -T "${file}" \
+              -H "Host: ${bucket}.s3.amazonaws.com" \
+              -H "Date: ${dateValue}" \
+              -H "Content-Type: ${contentType}" \
+              -H "Authorization: AWS ${s3Key}:${signature}" \
+              https://${bucket}.s3.amazonaws.com/${objectName}
+}
+
+for file in `find fcrepo-api-x-integration/target` ;
+do
+    putS3 $file
+done
